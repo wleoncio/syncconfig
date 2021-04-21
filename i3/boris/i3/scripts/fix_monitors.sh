9e9a9a666266eb00e52c366212dbd84e17566689
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-04-21 13:07:15 +0200

    Added conditional monitor configs for script

--- i3/boris/i3/scripts/fix_monitors.sh
+++ i3/boris/i3/scripts/fix_monitors.sh
@@ -1,2 +1,10 @@
-xrandr --output HDMI-0 --rotate right --mode 1920x1080 --pos 0x0 
-xrandr --output DP-0 --primary --mode 2560x1440 --rate 144.00 --pos 1700x520 
+#!/bin/bash
+
+if [ -z $1 ]
+then
+	xrandr --output DP-0 --mode 2560x1440 --rate 144.00 --primary --output HDMI-0 --rotate right --left-of DP-0 --mode 1920x1080
+else
+	xrandr --output HDMI-0 --rotate right --mode 1920x1080 --pos 0x0 
+	xrandr --output DP-0 --primary --mode 2560x1440 --rate 144.00 --pos 1700x520 
+fi
+
