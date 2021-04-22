4ccfafd6b03d9697996db98d0960904727aabc74
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-04-22 14:43:17 +0200

    Using native TV resolution

--- i3/boris/i3/scripts/fix_monitors.sh
+++ i3/boris/i3/scripts/fix_monitors.sh
@@ -2,8 +2,10 @@
 
 if [ -z $1 ]
 then
-	xrandr --output DP-0 --mode 2560x1440 --rate 144.00 --primary --output HDMI-0 --rotate right --left-of DP-0 --mode 1920x1080
+	# Top-aligned monitors, native resolutions
+	xrandr --output DP-0 --mode 2560x1440 --rate 144.00 --primary --output HDMI-0 --rotate right --left-of DP-0 --mode 1360x768
 else
+	# Bottom-aligned monitors, highest resolutions
 	xrandr --output HDMI-0 --rotate right --mode 1920x1080 --pos 0x0 
 	xrandr --output DP-0 --primary --mode 2560x1440 --rate 144.00 --pos 1700x520 
 fi
