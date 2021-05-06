1c9e0f075842c1f61d2625fad9bafd5299f8002b
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-05-06 13:31:41 +0200

    Changed location of wallpapers

--- i3/boris/i3/scripts/change_background.sh
+++ i3/boris/i3/scripts/change_background.sh
@@ -2,12 +2,12 @@
 
 if [ $1 == 0 ]
 then
-	nitrogen --head=0 --save --set-zoom-fill --random .config/variety/
+	nitrogen --head=0 --save --set-zoom-fill --random ~/Pictures/rid/
 elif [ $1 == 1 ]
 then
-	nitrogen --head=1 --save --set-zoom-fill --random .config/variety/Downloaded/reddit_www_reddit_com_r_Verticalwallpapers/
+	nitrogen --head=1 --save --set-zoom-fill --random ~/Pictures/rid/
 else
-	nitrogen --head=0 --save --set-zoom-fill --random .config/variety/
-	nitrogen --head=1 --save --set-zoom-fill --random .config/variety/Downloaded/reddit_www_reddit_com_r_Verticalwallpapers/
+	nitrogen --head=0 --save --set-zoom-fill --random ~/Pictures/rid/
+	nitrogen --head=1 --save --set-zoom-fill --random ~/Pictures/rid/
 fi
 
