ebb1acf353b0f1120f813e03ed0d38fa09710ac6
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-05-14 08:44:19 +0200

    Add feedback when changing/removing wallpaper

--- i3/boris/i3/scripts/change_background.sh
+++ i3/boris/i3/scripts/change_background.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Changing background 
 if [ $1 == 0 ]
 then
 	nitrogen --head=0 --save --set-zoom-fill --random ~/Pictures/rid/
@@ -11,3 +12,22 @@ else
 	nitrogen --head=1 --save --set-zoom-fill --random ~/Pictures/rid/
 fi
 
+# Informing name of new picture
+
+bg_config=.config/nitrogen/bg-saved.cfg
+
+# Collect background files
+bg_0=$(cat .config/nitrogen/bg-saved.cfg | grep file | tail -2 | head -1)
+bg_1=$(cat .config/nitrogen/bg-saved.cfg | grep file | tail -2 | tail -1)
+
+# Retrieving title
+title_0=$(echo $bg_0 | cut -d '/' -f 7)
+title_1=$(echo $bg_1 | cut -d '/' -f 7)
+
+if [ $1 == 0 ]
+then
+	eval notify-send --urgency low 'Setting' '"$title_0"'
+elif [ $1 == 1 ]
+then
+	eval notify-send --urgency low 'Setting' '"$title_1"'
+fi
--- i3/boris/i3/scripts/remove_background.sh
+++ i3/boris/i3/scripts/remove_background.sh
@@ -10,6 +10,11 @@ bg_1=$(cat .config/nitrogen/bg-saved.cfg | grep file | tail -2 | tail -1)
 bg_0=$(echo $bg_0 | cut -d '=' -f 2)
 bg_1=$(echo $bg_1 | cut -d '=' -f 2)
 
+# Retrieving title
+title_0=$(echo $bg_0 | cut -d '/' -f 7)
+title_1=$(echo $bg_1 | cut -d '/' -f 7)
+
+
 
 # Removing or printing the file
 
@@ -20,9 +25,11 @@ then
 	echo $bg_1
 elif [ $1 -eq 1 ]
 then
+	eval notify-send 'Removing "$title_1"' --urgency low
 	eval rm \""$bg_1"\"
 elif [ $1 -eq 0 ]
 then
+	eval notify-send 'Removing "$title_0"' --urgency low
 	eval rm \""$bg_0"\"
 fi
 
