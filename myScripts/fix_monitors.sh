49be0fb23e6146bf03dfe77c75ba5308e51ead17
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-09-01 15:02:51 +0200

    Changed to 1-monitor setup

--- myScripts/fix_monitors.sh
+++ myScripts/fix_monitors.sh
@@ -3,10 +3,10 @@
 if [ -z $1 ]
 then
 	# Top-aligned monitors, native resolutions
-	xrandr --output DP-0 --mode 2560x1440 --rate 144.00 --primary --output HDMI-0 --rotate right --left-of DP-0 --mode 1360x768
+	xrandr --output DP-0 --mode 2560x1440 --rate 144.00 --primary
 else
 	# Bottom-aligned monitors, highest resolutions
-	xrandr --output HDMI-0 --rotate right --mode 1920x1080 --pos 0x0 
-	xrandr --output DP-0 --primary --mode 2560x1440 --rate 144.00 --pos 1700x520 
+	xrandr --output HDMI-0 --rotate right --mode 1920x1080 --pos 0x0
+	xrandr --output DP-0 --primary --mode 2560x1440 --rate 144.00 --pos 1700x520
 fi
 
