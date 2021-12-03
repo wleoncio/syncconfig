68c387387961ae06e470527851f81a03c4fcdb77
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-12-03 12:37:06 +0100

    Fix monitors script now has selection + IMB config

--- myScripts/fix_monitors.sh
+++ myScripts/fix_monitors.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-if [ -z $1 ]
-then
-	# Top-aligned monitors, native resolutions
-	xrandr --output DP-0 --mode 2560x1440 --rate 144.00 --primary
-else
-	# Bottom-aligned monitors, highest resolutions
-	xrandr --output HDMI-0 --rotate right --mode 1920x1080 --pos 0x0
-	xrandr --output DP-0 --primary --mode 2560x1440 --rate 144.00 --pos 1700x520
-fi
-
+PS3="Select monitor configuration: "
+select config in imb home-top home-bottom quit
+do
+	case $config in
+		imb)
+			echo -e "The upright monitor at IMB tends to misbehave. Fixing..."
+			xrandr --output HDMI-3 --rotate normal
+			xrandr --output HDMI-3 --rotate left
+			nitrogen --restore
+			break
+			;;
+		home-top)
+			echo -e "Setting top-aligned monitors, native resolutions."
+			xrandr --output DP-0 --mode 2560x1440 --rate 144.00 --primary
+			break
+			;;
+		home-bottom)
+			echo -e "Setting Bottom-aligned monitors, highest resolutions."
+			xrandr --output HDMI-0 --rotate right --mode 1920x1080 --pos 0x0
+			xrandr --output DP-0 --primary --mode 2560x1440 --rate 144.00 --pos 1700x520
+			break
+			;;
+		quit)
+			echo -e "Ok, goodbye"
+			break
+			;;
+		*)
+			echo "\"$REPLY\" is not a valid option"
+			;;
+	esac
+done
