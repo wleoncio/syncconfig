abb62c05debcbf9ebd9a8cf55fe0ebb90671d549
Waldir Leoncio <w.l.netto@medisin.uio.no>
2022-08-10 11:17:54 +0200

    Improved timer

--- myScripts/timer.sh
+++ myScripts/timer.sh
@@ -1,9 +1,23 @@
-for i in `seq $1 -1 1`
+#!/bin/bash
+# Counts down from $1 seconds
+
+time=$1
+
+# Help
+if [ -z "$time" ]
+then
+	echo "Usage: timer.sh [seconds]"
+	exit 1
+fi
+
+# Countdown
+echo "$(date +%H:%M:%S) - Counting down from $time seconds"
+for i in `seq $time -1 1`
 do
 	echo -ne "\r$i "
 	sleep 1
 done
-echo -ne "\r0"
-sleep 1
-echo -e " - $1 seconds are over!"
+
+# Final feedback
+echo -ne "\r$(date +%H:%M:%S) - $time seconds are over!\n"
 notify-send "Time's up!"
