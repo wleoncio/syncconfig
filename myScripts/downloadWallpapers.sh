41ce082860bc11111d025fbf33a4c403e78b6e30
Waldir Leoncio <w.l.netto@medisin.uio.no>
2022-10-06 15:11:39 +0200

    DRYed wallpaper downloader

--- myScripts/downloadWallpapers.sh
+++ myScripts/downloadWallpapers.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-# Uses RedditImageGrab (https://github.com/HoverHell/RedditImageGrab) to download wallpapers
-subs='Verticalwallpapers HDwallpapers F1porn wallpaper wallpapers highreswallpaper 4kWallpaper threescreenwallpapers multiwall'
+# Uses RedditImageGrab (https://github.com/arshjat/RedditImageGrab) to download wallpapers
 
+# Variables
+subs='Verticalwallpapers HDwallpapers F1porn wallpaper wallpapers highreswallpaper 4kWallpaper'
+script=~/Programs/RedditImageGrab/redditdl.py
+outpath=~/Bilder/Wallpapers
+opts="--num 10 --sfw --filename-format png"
+
+# Looping through the subs to download wallpapers
 for sub in $subs
 do
-	python ~/Programs/RedditImageGrab/redditdl.py $sub ~/Bilder/Wallpapers --num 10 --sfw --update --filename-format png
+	python3 $script $sub $outpath $opts
 done
