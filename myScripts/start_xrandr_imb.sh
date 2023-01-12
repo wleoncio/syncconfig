6167a633f4e4114c2a01ae2e2e762f0135f90ef8
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-01-12 13:07:21 +0100

    Replaced HDMI-3 with DP-2 monitor

--- myScripts/start_xrandr_imb.sh
+++ myScripts/start_xrandr_imb.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-xrandr --output DP-1 --primary --output HDMI-3 --left-of DP-1
+xrandr --output DP-2 --mode 2560x1440
+sleep 2
+xrandr --output DP-2 --rotate left
+sleep 2
+xrandr --output DP-1 --primary --output DP-2 --left-of DP-1
 sleep 5
-xrandr --output HDMI-3 --mode 1920x1200
-sleep 2
-xrandr --output HDMI-3 --rotate left
-sleep 2
 nitrogen --restore
 
