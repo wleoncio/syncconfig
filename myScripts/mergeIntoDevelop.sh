a67a874361fe66cbbb521d7063f769c595e7ac12
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-01-20 15:47:08 +0100

    Added colors to script

--- myScripts/mergeIntoDevelop.sh
+++ myScripts/mergeIntoDevelop.sh
@@ -3,7 +3,8 @@
 
 feature_branch=$(eval git branch --show-current)
 echo "Merging $feature_branch into develop. Did you remember to:"
-read -p "Incremented the build version number?"
-read -p "Updated NEWS.md?"
+read -p $'Added \e[4;31munit tests\e[0m for new code?'
+read -p $'Incremented the build \e[4;31mversion\e[0m number?'
+read -p $'Updated \e[4;31mNEWS\e[0m.md?'
 git checkout develop
 git merge $feature_branch
