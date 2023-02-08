9bd517f5bd022ea47e2c3a358e15b612c08fcce5
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-02-08 07:04:32 +0100

    Adding feedback

--- myScripts/workOnIssue.sh
+++ myScripts/workOnIssue.sh
@@ -2,6 +2,10 @@
 # Starts working on a GitHub issue
 
 issueNumber=$1
+
+echo "Self-assigning issue on GitHub"
 gh issue edit "$issueNumber" --add-assignee @me
+
+echo "Crating and checking out branch"
 git checkout -b issue-"$issueNumber"
 
