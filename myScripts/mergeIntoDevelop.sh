e0dd157d8f32402d5a291bd5493af30bc520049a
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-08-31 09:03:50 +0200

    Grammar fix

--- myScripts/mergeIntoDevelop.sh
+++ myScripts/mergeIntoDevelop.sh
@@ -7,9 +7,9 @@ echo "Unstaged modifications:"
 git status --short
 
 echo "Merging $feature_branch into develop. Did you remember to:"
-read -p $'\e[4;31mSquashed\e[0m commits on the feature branch?'
-read -p $'Added \e[4;31munit tests\e[0m for new code?'
-read -p $'Incremented the build \e[4;31mversion\e[0m number?'
-read -p $'Updated \e[4;31mNEWS\e[0m.md?'
+read -p $'\e[4;31mSquash\e[0m commits on the feature branch?'
+read -p $'Add \e[4;31munit tests\e[0m for new code?'
+read -p $'Increment the build \e[4;31mversion\e[0m number?'
+read -p $'Update \e[4;31mNEWS\e[0m.md?'
 git checkout develop
 git merge $feature_branch
