c83fb9a34740fb14c840f5a8150ff59350fbda0d
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-08-31 09:02:33 +0200

    Added git status check before merge check

--- myScripts/mergeIntoDevelop.sh
+++ myScripts/mergeIntoDevelop.sh
@@ -2,7 +2,11 @@
 # Script to merge feature branches into develop
 
 feature_branch=$(eval git branch --show-current)
-echo "Merging $feature_branch into develop. Did you remember to:"i
+
+echo "Unstaged modifications:"
+git status --short
+
+echo "Merging $feature_branch into develop. Did you remember to:"
 read -p $'\e[4;31mSquashed\e[0m commits on the feature branch?'
 read -p $'Added \e[4;31munit tests\e[0m for new code?'
 read -p $'Incremented the build \e[4;31mversion\e[0m number?'
