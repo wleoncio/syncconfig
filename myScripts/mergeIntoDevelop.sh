afed625602e7f93548026addbd376f68c6910374
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-10-17 14:07:21 +0200

    Added versioning option to develop merge script

--- myScripts/mergeIntoDevelop.sh
+++ myScripts/mergeIntoDevelop.sh
@@ -1,21 +1,41 @@
-#! /bin/bash
+#!/usr/bin/bash
 # Script to merge feature branches into develop
+
+# Usage
+echo "Usage: $(basename $0) [options]" 2>&1
+echo "	-t	Skips unit test coverage check"
+echo "	-v	Skips version bump"
+echo ""
+
+# Parsing options
+covr=true
+version=true
+while getopts "tv" option; do
+	case "$option" in
+		t) covr=false ;;
+		v) version=false ;;
+		?) echo "Invalid option: -${OPTARG}" ;;
+	esac
+done
 
 feature_branch=$(eval git branch --show-current)
 
 echo "Unstaged modifications:"
 git status --short
 
-if [ "$1" != "nocov" ]
-then
+if [ $covr == true ]; then
 	echo "Package coverage on merge"
-	Rscript -e "covr::percent_coverage(covr::package_coverage())"
+	Rscript -e "cat(covr::percent_coverage(covr::package_coverage()))"
 fi
 
 echo "Merging $feature_branch into develop. Did you remember to:"
 read -p $'\e[4;31mSquash\e[0m commits on the feature branch?'
 read -p $'Add \e[4;31munit tests\e[0m for new code?'
-read -p $'Increment the build \e[4;31mversion\e[0m number?'
 read -p $'Update \e[4;31mNEWS\e[0m.md?'
 git checkout develop
 git merge $feature_branch --log
+
+if [ $version == true ]; then
+	echo "Incrementing build number"
+	Rscript -e "usethis::use_version('build')"
+fi
