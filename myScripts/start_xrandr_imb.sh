0a7102b6bfc6255deb2288b33ebccceb88d04f89
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-02-20 11:34:31 +0100

    Repositioning DP-2

--- myScripts/start_xrandr_imb.sh
+++ myScripts/start_xrandr_imb.sh
@@ -3,7 +3,7 @@ xrandr --output DP-2 --mode 2560x1440
 sleep 2
 xrandr --output DP-2 --rotate left
 sleep 2
-xrandr --output DP-1 --primary --output DP-2 --left-of DP-1
+xrandr --output DP-1 --primary --pos 1440x474 --output DP-2 --pos 0x0
 sleep 5
 nitrogen --restore
 
