7300c76080f1bc4bdc048406a64b0dd384e03038
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-02-22 14:45:30 +0100

    Simplified output
    Command output is already pretty self-explanatory

--- myScripts/resumeWorkOnIssue.sh
+++ myScripts/resumeWorkOnIssue.sh
@@ -3,24 +3,8 @@
 
 issue_number=$1
 
-echo "#######################"
-echo "# Checking out branch #"
-echo "#######################"
-echo ""
 git checkout issue-"$issue_number"
-
-echo ""
-echo "#########################"
-echo "# Rebasing with develop #"
-echo "#########################"
-echo ""
 git rebase develop
-
-echo ""
-echo "#########################"
-echo "# Updating build number #"
-echo "#########################"
-echo ""
 Rscript -e "usethis::use_version('dev')"
 new_dev_version=$(cat DESCRIPTION | grep Version: | cut -d " " -f 2)
 git commit --all --message "Increment version number to "$new_dev_version""
