92bfb638169568a7da18a7f21925f31b74f9cd8c
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-09-09 15:16:00 +0200

    Syncing Fish config (closes #24)

---
+++ config/fish/config.fish
@@ -0,0 +1,26 @@
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+	source $HOME/.bash_aliases
+	fish_add_path $HOME/Programs/syncconfig/
+end
+
+starship init fish | source
+zoxide init --cmd cd fish | source
+
+# Fish vim config
+fish_vi_key_bindings
+set fish_cursor_default block blink
+# Emulates vim's cursor shape behavior
+# Set the normal and visual mode cursors to a block
+set fish_cursor_default block blink
+# Set the insert mode cursor to a line
+set fish_cursor_insert line blink
+# Set the replace mode cursors to an underscore
+set fish_cursor_replace_one underscore blink
+set fish_cursor_replace underscore blink
+# Set the external cursor to a line. The external cursor appears when a command is started.
+# The cursor shape takes the value of fish_cursor_default when fish_cursor_external is not specified.
+set fish_cursor_external line blink
+# The following variable can be used to configure cursor shape in
+# visual mode, but due to fish_cursor_default, is redundant here
+set fish_cursor_visual block blink
