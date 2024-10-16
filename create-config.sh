23eb2019248bb3e5d32f389bfaa71d20dedd3c3c
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-10-16 11:58:40 +0200

    Added script to create config file (closes #22)

---
+++ create-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Creates a syncconfig.conf file
+
+# Check if the file exists
+if [ -f "$HOME/.config/syncconfig.conf" ]; then
+  # Prompt to overwrite
+  read -p "syncconfig.conf already exists. Overwrite? (y/N) " overwrite
+  if [ "$overwrite" != "y" ]; then
+    echo "Exiting..."
+    exit 1
+  fi
+fi
+
+# Ask for location of sync directory
+read -p "Enter the full path to the sync directory: " syncdir
+echo "location=$syncdir" > $HOME/.config/syncconfig.conf
