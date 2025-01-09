0596874c80b65bc9bf070ba80c49c62c5c0acd30
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-01-09 14:44:12 +0100

    Added script to choose audio output of scrcpy

---
+++ myScripts/scrcpy_audio.sh
@@ -0,0 +1,8 @@
+read -p "Select audio destination ([m]obile, [d]esktop): " output
+COMMON_ARGS=--always-on-top --keyboard=uhid
+if [ $output = 'm' ]; then
+	scrcpy --no-audio $COMMON_ARGS
+else
+ 	scrcpy $COMMON_ARGS
+fi
+
