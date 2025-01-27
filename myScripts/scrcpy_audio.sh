39383ce39e84e8f9f21c43142d23e9008300c73f
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-01-27 14:34:00 +0100

    Added sleep to scrcpy
    Allows time to hide window

--- myScripts/scrcpy_audio.sh
+++ myScripts/scrcpy_audio.sh
@@ -1,5 +1,6 @@
 read -p "Select audio destination ([m]obile, [d]esktop): " output
 COMMON_ARGS="--always-on-top --keyboard=uhid --power-off-on-close"
+sleep 2
 if [ $output = 'm' ]; then
 	scrcpy --no-audio $COMMON_ARGS
 else
