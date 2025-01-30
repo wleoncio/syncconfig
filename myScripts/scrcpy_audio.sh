d3e45a300a12b837831b80b9899d943730954e07
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-01-30 10:41:07 +0100

    Auto-hide script window

--- myScripts/scrcpy_audio.sh
+++ myScripts/scrcpy_audio.sh
@@ -1,9 +1,12 @@
 read -p "Select audio destination ([m]obile, [d]esktop): " output
-COMMON_ARGS="--always-on-top --keyboard=uhid --power-off-on-close"
-sleep 2
+COMMON_ARGS="--always-on-top --keyboard=aoa --power-off-on-close"
+
+# Press (1) and release (0) Super (125) + H (35)
+# See /usr/include/linux/input-event-codes.h for more codes
+YDOTOOL_SOCKET="$HOME/.ydotool_socket" ydotool key 125:1 35:1 35:0 125:0
+
 if [ $output = 'm' ]; then
 	scrcpy --no-audio $COMMON_ARGS
 else
  	scrcpy $COMMON_ARGS
 fi
-
