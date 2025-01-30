9e8c991f722da5e338953563343cf4d10d3e0cd9
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-01-30 13:31:04 +0100

    Move scrcpy terminal to left monitor

--- myScripts/scrcpy_audio.sh
+++ myScripts/scrcpy_audio.sh
@@ -1,8 +1,11 @@
 read -p "Select audio destination ([m]obile, [d]esktop): " output
 COMMON_ARGS="--keyboard=aoa --power-off-on-close"
 
-# Press (1) and release (0) Super (125) + H (35)
 # See /usr/include/linux/input-event-codes.h for more codes
+# Press Super+Shift+left to move window to left monitor
+YDOTOOL_SOCKET="$HOME/.ydotool_socket" ydotool key 125:1 42:1 105:1 125:0 42:0 105:0
+
+# Press (1) and release (0) Super (125) + H (35) to hide window
 YDOTOOL_SOCKET="$HOME/.ydotool_socket" ydotool key 125:1 35:1 35:0 125:0
 
 if [ $output = 'm' ]; then
