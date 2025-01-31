988152df51f817dfaf274690b75f8107d273b5f1
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-01-31 10:41:57 +0100

    Starts ydotoold if necessary

--- myScripts/scrcpy_audio.sh
+++ myScripts/scrcpy_audio.sh
@@ -1,7 +1,17 @@
+#!/bin/bash
+
 read -p "Select audio destination ([m]obile, [d]esktop): " output
 COMMON_ARGS="--keyboard=aoa --power-off-on-close"
 
+# Starting ydotoold
+if [ $(ps auxf | grep ydotoold | wc -l) -eq 1 ]; then
+	echo "Starting ydotoold"
+	sudo -b ydotoold --socket-path="$HOME/.ydotool_socket" --socket-own="$(id -u):$(id -g)"
+fi
+
+# Simulating keypresses
 # See /usr/include/linux/input-event-codes.h for more codes
+
 # Press Super+Shift+left to move window to left monitor
 YDOTOOL_SOCKET="$HOME/.ydotool_socket" ydotool key 125:1 42:1 105:1 125:0 42:0 105:0
 
