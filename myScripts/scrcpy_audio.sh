c217e7b790fb048ba9db31dd6707e7d5ac9992c2
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-02-10 16:50:14 +0100

    Added sleep to scrcpy
    Allows time to hide window

--- myScripts/scrcpy_audio.sh
+++ myScripts/scrcpy_audio.sh
@@ -15,6 +15,8 @@ fi
 # Press Super+Shift+left to move window to left monitor
 YDOTOOL_SOCKET="$HOME/.ydotool_socket" ydotool key 125:1 42:1 105:1 125:0 42:0 105:0
 
+sleep 0.3
+
 # Press (1) and release (0) Super (125) + H (35) to hide window
 YDOTOOL_SOCKET="$HOME/.ydotool_socket" ydotool key 125:1 35:1 35:0 125:0
 
