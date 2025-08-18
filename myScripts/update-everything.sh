176df2163ecb98d6d40be170c622f5e28e48ae7a
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-08-18 09:21:27 +0200

    Implement snap, flatpak and gh-cli upgrade

--- myScripts/update-everything.sh
+++ myScripts/update-everything.sh
@@ -12,13 +12,24 @@ elif [ -e /bin/nala ]; then
 	package_manager_command="nala upgrade --assume-yes"
 fi
 
- # TODO: check if snap exists
- # TODO: check if flatpak exists
- # TODO: check if gh exists
-
 # Checking for upgrades
 sudo --validate
 if [ -n "$package_manager_name" ]; then 
 	echo -e "# " $package_manager_name "\n"
 	$package_manager_command
+fi
+
+if [ -e /bin/snap ]; then
+	echo -e "# Snap\n"
+	sudo snap refresh
+fi
+
+if [ -e /bin/flatpak ]; then
+	echo -e "# Flatpak\n"
+	flatpak update --assumeyes
+fi
+
+if [ -e /bin/gh ]; then
+	echo -e "# GitHub CLI\n"
+	gh extension upgrade --all
 fi
