130664b54f2c975347d4176660a0e411e6a83952
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-10-06 09:39:41 +0200

    Add control over wildcards on search term

--- myScripts/kd.sh
+++ myScripts/kd.sh
@@ -4,7 +4,7 @@ version="0.1.0 licensed under GPLv3"
 usage() {
   cat << EOU
 Usage:
-  $(basename $0) SEARCH_TERM [-l LOCATION | --location LOCATION] [--ignore-case]
+  $(basename $0) SEARCH_TERM [-l LOCATION | --location LOCATION] [--ignore-case] [--starts-with] [--ends-with]
   $(basename $0) -h | --help
   $(basename $0) --version
 
@@ -16,12 +16,16 @@ Arguments:
 Options:
   -l LOCATION, --location LOCATION  Directory to search in [default: $HOME]
   --ignore-case                     Perform case-insensitive search
+  --starts-with                     Search for files that start with SEARCH_TERM
+  --ends-with                       Search for files that end with SEARCH_TERM
   -h --help                         Print this help and exit
   --version                         Print version and exit
 
 Examples:
   $(basename $0) .bashrc
   $(basename $0) bASh --ignore-case -l /usr/bin
+  $(basename $0) bash --starts-with
+  $(basename $0) bash --ends-with
 
 Send bug reports and feature requests to: https://github.com/wleoncio/syncconfig/issues
 EOU
@@ -33,7 +37,9 @@ eval "$(docopts -A ARGS -h "$(usage)" -V "$version" : "$@")"
 SEARCH_TERM="${ARGS['SEARCH_TERM']}"
 LOCATION="${ARGS['--location']:-${ARGS['-l']:-$HOME}}"
 CASE=$([ "${ARGS['--ignore-case']}" = true ] && echo iname || echo name)
+PREFIX=$([ "${ARGS['--ends-with']}" = true ] && echo "*" || echo "")
+SUFFIX=$([ "${ARGS['--starts-with']}" = true ] && echo "*" || echo "")
 
-echo -e "Searching for \"$SEARCH_TERM\" in $LOCATION\n"
+echo -e "Searching for \"$PREFIX$SEARCH_TERM$SUFFIX\" in $LOCATION\n"
 
-find $LOCATION -$CASE *$SEARCH_TERM* -print 2>/dev/null
+find $LOCATION -$CASE $PREFIX$SEARCH_TERM$SUFFIX -print 2>/dev/null
