1ccbd9f26f306d897f9c4b154c66deddf541f18d
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-10-06 08:59:32 +0200

    Change `LOCATION` from arg to flag
    According to CLI guidelines, "it's a bit more typing, but it makes it much clearer what is going on. It also makes it easier to make changes to how you accept input in the future. Sometimes when using args, it's impossible to add new input without breaking existing behavior or creating ambiguity".

--- myScripts/kd.sh
+++ myScripts/kd.sh
@@ -4,7 +4,7 @@ version="0.1.0 licensed under GPLv3"
 usage() {
   cat << EOU
 Usage:
-  $(basename $0) SEARCH_TERM [LOCATION] [--ignore-case]
+  $(basename $0) SEARCH_TERM [--location LOCATION] [--ignore-case]
   $(basename $0) -h | --help
   $(basename $0) --version
 
@@ -12,16 +12,16 @@ A find wrapper to conveniently and cleanly search for files and directories.
 
 Arguments:
   SEARCH_TERM      term to search for (wildcards * are allowed)
-  LOCATION         directory to search in [default: $HOME]
 
 Options:
-  --ignore-case  Perform case-insensitive search
-  -h --help      Print this help and exit
-  --version      Print version and exit
+  --location LOCATION  Directory to search in [default: $HOME]
+  --ignore-case       Perform case-insensitive search
+  -h --help           Print this help and exit
+  --version           Print version and exit
 
 Examples:
   $(basename $0) .bashrc
-  $(basename $0) bASh --ignore-case /usr/bin
+  $(basename $0) bASh --ignore-case --location /usr/bin
 
 Send bug reports and feature requests to: https://github.com/wleoncio/syncconfig/issues
 EOU
@@ -30,9 +30,8 @@ EOU
 # Setting variables
 eval "$(docopts -A ARGS -h "$(usage)" -V "$version" : "$@")"
 
-# Get MONTH and YEAR from ARGS, default to current if not provided
 SEARCH_TERM="${ARGS['SEARCH_TERM']}"
-LOCATION="${ARGS['LOCATION']:-$HOME}"
+LOCATION="${ARGS['--location']:-$HOME}"
 
 if [ "${ARGS['--ignore-case']}" = true ]; then
   opts=iname
@@ -42,4 +41,5 @@ fi
 
 echo -e "Searching for \"$SEARCH_TERM\" in $LOCATION\n"
 
+find $LOCATION -$opts *$SEARCH_TERM* -print 2>/dev/null
 find $LOCATION -$opts *$SEARCH_TERM* -print 2>/dev/null
